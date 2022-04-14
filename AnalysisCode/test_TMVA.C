0807325cb8952165d002b6869975a19d78d755d0
Threshic <paul.vaucelle@etu.unistra.fr>
2022-04-14 16:27:22 +0200

    Better TMVA // TMVA test and training samples

--- AnalysisCode/test_TMVA.C
+++ AnalysisCode/test_TMVA.C
@@ -19,17 +19,17 @@ void doTraining(){
     TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", outputFile,
                                                "!V:!Silent:Color:DrawProgressBar:AnalysisType=Classification");
     TMVA::DataLoader *dataloader=new TMVA::DataLoader("dataset");
-    dataloader->AddVariable( "mva_track_firstHit_x", 'F' );
-    dataloader->AddVariable( "mva_track_firstHit_y", 'F' );
-    dataloader->AddVariable( "mva_track_firstHit_z", 'F' );
-    dataloader->AddVariable( "mva_track_dxy", 'F' );
-    dataloader->AddVariable( "mva_track_dxyError", 'F' );
-    dataloader->AddVariable( "mva_track_dz", 'F' );
-    dataloader->AddVariable( "mva_track_dzError", 'F' );
+    // dataloader->AddVariable( "mva_track_firstHit_x", 'F' );//exclude variables that higly depend on the flight distance
+    // dataloader->AddVariable( "mva_track_firstHit_y", 'F' );//exclude variables that higly depend on the flight distance
+    // dataloader->AddVariable( "mva_track_firstHit_z", 'F' );//exclude variables that higly depend on the flight distance
+    // dataloader->AddVariable( "mva_track_dxy", 'F' );
+    // dataloader->AddVariable( "mva_track_dxyError", 'F' );
+    // dataloader->AddVariable( "mva_track_dz", 'F' );
+    // dataloader->AddVariable( "mva_track_dzError", 'F' );
     dataloader->AddVariable( "mva_track_pt", 'F' );
     dataloader->AddVariable( "mva_track_eta", 'F' );
     dataloader->AddVariable( "mva_track_nchi2", 'F' );
-    dataloader->AddVariable( "mva_track_nhits", 'F' );
+    // dataloader->AddVariable( "mva_track_nhits", 'F' );//exclude variables that higly depend on the flight distance
     dataloader->AddVariable( "mva_track_algo", 'F');
 
     //added by Paul
@@ -38,51 +38,54 @@ void doTraining(){
     dataloader->AddVariable( "mva_ntrk10", 'F');
     dataloader->AddVariable( "mva_ntrk20", 'F');
     dataloader->AddVariable( "mva_ntrk30", 'F');
-    dataloader->AddVariable( "mva_ntrk40", 'F');
-    dataloader->AddVariable( "mva_ntrk1020", 'F');
-    dataloader->AddVariable( "mva_ntrk2030", 'F');
-    dataloader->AddVariable( "mva_dzSig", 'F');
+    // dataloader->AddVariable( "mva_ntrk40", 'F');
+    // dataloader->AddVariable( "mva_ntrk1020", 'F');
+    // dataloader->AddVariable( "mva_ntrk2030", 'F');
+    // dataloader->AddVariable( "mva_dzSig", 'F');
     dataloader->AddVariable( "mva_drSig", 'F');
-    dataloader->AddVariable( "mva_ddSig", 'F');
-    dataloader->AddVariable( "mva_ValTIBHit", 'F');
-    dataloader->AddVariable( "mva_ValTOBHit", 'F');
-    dataloader->AddVariable( "mva_ValPixBarHit", 'F');
+    // dataloader->AddVariable( "mva_ddSig", 'F');
+    // dataloader->AddVariable( "mva_ValTIBHit", 'F');//exclude variables that higly depend on the flight distance
+    // dataloader->AddVariable( "mva_ValTOBHit", 'F');//exclude variables that higly depend on the flight distance
+    // dataloader->AddVariable( "mva_ValPixBarHit", 'F');//exclude variables that higly depend on the flight distance
     dataloader->AddVariable( "mva_track_isinjet", 'F');
-    dataloader->AddVariable( "mva_ntrk40XX;", 'F');
-    dataloader->AddVariable( "mva_nValTECHHit", 'F');
+    // dataloader->AddVariable( "mva_ntrk40XX;", 'F');
+    // dataloader->AddVariable( "mva_nValTECHHit", 'F');
    
     
     //end of added by Paul
     
     // global event weights per tree (see below for setting event-wise weights)
-    Double_t signalWeight     = 1.0;
-    Double_t backgroundWeight = 1.0;
+    Double_t signalWeight     = 1.0;//1.0
+    Double_t backgroundWeight = 1.0;//1.0
     dataloader->AddSignalTree    ( signalTree,     signalWeight );
     dataloader->AddBackgroundTree( background, backgroundWeight );
 
     // Set individual event weights (the variables must exist in the original TTree)
-    // -  for signal    : `dataloader->SetSignalWeightExpression    ("weight1*weight2");`
-    // -  for background: `dataloader->SetBackgroundWeightExpression("weight1*weight2");`
-    //dataloader->SetBackgroundWeightExpression( "weight" );
+    // -  for signal    : `
+    // dataloader->SetSignalWeightExpression("");
+    // -  for background: `
+    // dataloader->SetBackgroundWeightExpression("");
+    //dataloader->SetBackgroundWeightExpression( "" );
     // Apply additional cuts on the signal and background samples (can be different)
     TCut mycuts = ""; // for example: TCut mycuts = "abs(var1)<0.5 && abs(var2-0.5)<1";
     TCut mycutb = ""; // for example: TCut mycutb = "abs(var1)<0.5";
-    
+    // abs(mva_track_eta)>1.5
+    // abs(mva_track_eta)<1.5
     //Jeremy Andrea
     //factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTG",
     //     "!H:!V:NTrees=1000:MinNodeSize=2.5%:BoostType=Grad:Shrinkage=0.10:UseBaggedBoost:BaggedSampleFraction=0.5:nCuts=20:MaxDepth=2" );
     
     dataloader->PrepareTrainingAndTestTree( mycuts, mycutb,
-                                        "nTrain_Signal=20000:nTrain_Background=20000:SplitMode=Random:NormMode=NumEvents:!V" );
+                                        "nTrain_Signal=10000:nTrain_Background=10000:SplitMode=Random:NormMode=NumEvents:!V" );
     //Duja
     // factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDT",
     //      "!H:!V:NTrees=850:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" );
 
     //Paul//
     factory->BookMethod( dataloader, TMVA::Types::kBDT, "BDTG",
-         "!H:!V:NTrees=1000:MinNodeSize=2.5%:MaxDepth=3:BoostType=Grad:UseBaggedBoost=False:GradBaggingFraction=0.6:Shrinkage=0.1:SeparationType=GiniIndex:nCuts=20:UseYesNoLeaf=True:UseRandomisedTrees=True");//Possible overtraining
+         "!H:!V:NTrees=1000:MinNodeSize=2.5%:MaxDepth=3:BoostType=Grad:UseBaggedBoost=True:GradBaggingFraction=0.6:Shrinkage=0.1:SeparationType=GiniIndex:nCuts=20:UseYesNoLeaf=True:UseRandomisedTrees=False:DoBoostMonitor=True");//Possible overtraining
             //////Parameter tested independantly: (with S= 10 000 and bkg =10000)
-                        // Best ROC obtained is 0.885 (20000,20000, NTrees=1000:MinNodeSize=2.5%:MaxDepth=5:BoostType=Grad:UseBaggedBoost=False:GradBaggingFraction=0.6:Shrinkage=0.1:SeparationType=GiniIndex:nCuts=20:UseYesNoLeaf=True:UseRandomisedTrees=True)
+                        // Best ROC obtained is 0.894 (20000,20000, NTrees=1000:MinNodeSize=2.5%:MaxDepth=5:BoostType=Grad:UseBaggedBoost=True:GradBaggingFraction=0.6:Shrinkage=0.1:SeparationType=GiniIndex:nCuts=20:UseYesNoLeaf=True:UseRandomisedTrees=False:DoBoostMonitor=True"
                         // 
                         //"!H:!V:NTrees=1000:MinNodeSize=5%:MaxDepth=2:BoostType=Grad:UseBaggedBoost=True:GradBaggingFraction=0.6:Shrinkage=0.1:SeparationType=GiniIndex:nCuts=20:UseYesNoLeaf=True:UseRandomisedTrees=True"
                         //Putting max depth to higher values does not increase the performances.
@@ -97,7 +100,7 @@ void doTraining(){
                         // Pour le test de Kolmogorov, voir les conditions : https://root.cern/doc/master/classTH1.html#aeadcf087afe6ba203bcde124cfabbee4
                         //General Comment: all the parameters above can be less "strict" to save computational time if needed since few parameters can change
                         // by a large value the ROC value 
-            //////Parameter tested :
+            //////Parameter tested : Adaboost, slightly worse
 
    // Now you can tell the factory to train, test, and evaluate the MVAs
    //
