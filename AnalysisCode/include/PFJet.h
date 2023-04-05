ee9920105ff4422bbd3b6359cdb30c4a6ef11bc6
Paul Vaucelle <paul.vaucelle@etu.unistra.fr>
2023-04-05 15:17:09 +0200

    04/04/23
    the commented part is for reco datatier

--- AnalysisCode/include/PFJet.h
+++ AnalysisCode/include/PFJet.h
@@ -6,7 +6,7 @@
 
 //#include "include/TreeFormat.h"
 #include "TreeFormat.h"
-
+// #include "TreeFormatMINIAOD.h"
 class PFJet {
 
     public:
@@ -15,24 +15,41 @@ class PFJet {
         virtual ~PFJet();
         //int ID()                                         {return _ID;};
         int idx()          {return _idx;};
-        float  e()         {return *_AK4Slimmedjet_E;};
-        float  pt()        {return *_AK4Slimmedjet_pt;};
-        float  eta()       {return *_AK4Slimmedjet_eta;};
-        float  phi()       {return *_AK4Slimmedjet_phi;};
-        int idxTrack()     {return *_AK4Slimmedjet_idxTrack;};
-        
+        // float  e()         {return *_AK4Slimmedjet_E;};
+        // float  pt()        {return *_AK4Slimmedjet_pt;};
+        // float  eta()       {return *_AK4Slimmedjet_eta;};
+        // float  phi()       {return *_AK4Slimmedjet_phi;};
+        // int idxTrack()     {return *_AK4Slimmedjet_idxTrack;};
+
+//reco with miniaod names
+        float  e()         {return *_jet_E;};
+        float  pt()        {return *_jet_pt;};
+        float  eta()       {return *_jet_eta;};
+        float  phi()       {return *_jet_phi;};
+        // int idxTrack()     {return *_AK4Slimmedjet_idxTrack;};
+//----------------------      
+        float  mini_e()         {return *_jet_E;};
+        float  mini_pt()        {return *_jet_pt;};
+        float  mini_eta()       {return *_jet_eta;};
+        float  mini_phi()       {return *_jet_phi;};
+        // int idxTrack()     {return *_AK4Slimmedjet_idxTrack;};
         
 
         void read(int theIdx, TreeFormat * theContainer);
-
+        // void read(int theIdx, TreeFormatMINIAOD * theContainer);
     protected:
         //int _ID;
         int     _idx;
-        float   *_AK4Slimmedjet_E;
-        float   *_AK4Slimmedjet_pt;
-        float   *_AK4Slimmedjet_eta;
-        float   *_AK4Slimmedjet_phi;
-        int     *_AK4Slimmedjet_idxTrack;
+        // float   *_AK4Slimmedjet_E;
+        // float   *_AK4Slimmedjet_pt;
+        // float   *_AK4Slimmedjet_eta;
+        // float   *_AK4Slimmedjet_phi;
+        // int     *_AK4Slimmedjet_idxTrack;
+
+        float   *_jet_E;
+        float   *_jet_pt;
+        float   *_jet_eta;
+        float   *_jet_phi;
         
 
 };
