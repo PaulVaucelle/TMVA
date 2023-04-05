74e1149d8deeab7e9fb6b34b23661438fceacef9
Paul Vaucelle <paul.vaucelle@etu.unistra.fr>
2023-04-05 15:14:39 +0200

    04/04/23
    the commented part is for reco

--- AnalysisCode/src/PFJet.cc
+++ AnalysisCode/src/PFJet.cc
@@ -15,13 +15,17 @@ void PFJet::read(int theIdx, TreeFormat * theContainer)
     _idx = theIdx;
 
 
-    //_muon_pt=theContainer->tree_muon_pt->at(_idx) ;
-    _AK4Slimmedjet_E        = &theContainer->tree_AK4Slimmedjet_E->at(_idx) ;
-    _AK4Slimmedjet_pt       = &theContainer->tree_AK4Slimmedjet_pt->at(_idx) ;
-    _AK4Slimmedjet_eta      = &theContainer->tree_AK4Slimmedjet_eta->at(_idx) ;
-    _AK4Slimmedjet_phi      = &theContainer->tree_AK4Slimmedjet_phi->at(_idx) ;
-    _AK4Slimmedjet_idxTrack = &theContainer->tree_AK4Slimmedjet_idxTrack->at(_idx) ;
+    // _AK4Slimmedjet_E        = &theContainer->tree_AK4Slimmedjet_E->at(_idx) ;
+    // _AK4Slimmedjet_pt       = &theContainer->tree_AK4Slimmedjet_pt->at(_idx) ;
+    // _AK4Slimmedjet_eta      = &theContainer->tree_AK4Slimmedjet_eta->at(_idx) ;
+    // _AK4Slimmedjet_phi      = &theContainer->tree_AK4Slimmedjet_phi->at(_idx) ;
+    // _AK4Slimmedjet_idxTrack = &theContainer->tree_AK4Slimmedjet_idxTrack->at(_idx) ;
 
+
+    _jet_E= &theContainer->tree_jet_E->at(_idx);
+    _jet_pt= &theContainer->tree_jet_pt->at(_idx);
+    _jet_eta= &theContainer->tree_jet_eta->at(_idx);
+    _jet_phi= &theContainer->tree_jet_phi->at(_idx);
 }
 
 
